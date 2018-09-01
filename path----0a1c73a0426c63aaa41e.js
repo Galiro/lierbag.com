a0227cd063dfe2928ded910776a77fd981b57b42
Gabriel Rosa <galiro@icloud.com>
2018-09-01 16:47:28 -0400

    Updates

---
+++ path----0a1c73a0426c63aaa41e.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{104:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Gabriel Rosa"}}},layoutContext:{}}}});
+//# sourceMappingURL=path----0a1c73a0426c63aaa41e.js.map
---
+++ path---blog-my-first-post-9e675da34d82fec82cc7.js
@@ -0,0 +1,2 @@
+webpackJsonp([0xdb82e3537dc5],{327:function(t,o){t.exports={data:{markdownRemark:{html:"<h2>Yo</h2>",frontmatter:{date:"November 07, 2017",path:"/blog/my-first-post",title:"My first blog post"}}},pathContext:{}}}});
+//# sourceMappingURL=path---blog-my-first-post-9e675da34d82fec82cc7.js.map
